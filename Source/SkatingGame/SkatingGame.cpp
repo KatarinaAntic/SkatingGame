49740c1a0dbcae52ce73546d5a2a2f2ebe899be3
Katarina Antic <katarina_antic@hotmail.com>
2025-04-10 22:42:08 +0200

    Add project files.

---
+++ Source/SkatingGame/SkatingGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkatingGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkatingGame, "SkatingGame" );
